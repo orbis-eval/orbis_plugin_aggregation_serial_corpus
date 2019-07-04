99b8ea67ea1cc651dd035b0eccc95f053d1c5ba1
fabod <2189668+fabod@users.noreply.github.com>
2019-07-04 10:30:20 +0200

    first commit

---
+++ orbis_plugin_aggregation_serial_corpus/main.py
@@ -0,0 +1,25 @@
+import glob
+import os
+from orbis import app
+
+
+class SerialCorpus(object):
+
+    def __init__(self, rucksack, path=None):
+        super(SerialCorpus, self).__init__()
+        self.rucksack = rucksack
+        if not path:
+            self.corpus_path = self.rucksack.open['config']['corpus_path']
+        else:
+            self.corpus_path = path
+
+    def run(self):
+        corpus = {}
+        for file_dir in glob.glob(os.path.abspath(os.path.join(self.corpus_path, '*.txt'))):
+            try:
+                file_number = file_dir.split('/')[-1].split('.')[0]
+                with open(file_dir) as open_file:
+                    corpus[file_number] = open_file.read()
+            except Exception as exception:
+                app.logger.error(f"Corpus file empty: {file_dir} ({exception})")
+        return corpus
